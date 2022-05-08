9b1b0b6a7f59da0594e23f2d6a7a738beea870d7
SOFe <sofe2038@gmail.com>
2022-05-08 21:07:58 +0800

    Added tests for intersecting isotope exclusion

--- src/world/scheduler/tests.rs
+++ src/world/scheduler/tests.rs
@@ -6,12 +6,12 @@ use parking_lot::Once;
 use super::*;
 use crate::test_util::AntiSemaphore;
 use crate::world::tracer;
-use crate::{system, world};
+use crate::{comp, system, world, TestArch};
 
 // Repeat concurrent tests to increase the chance of catching random bugs.
 // However, do not rely on test repetitions to assert for behavior;
 // use more synchronization where practical.
-const CONCURRENT_TEST_REPETITIONS: usize = 100;
+const CONCURRENT_TEST_REPETITIONS: usize = if option_env!("RUST_LOG").is_some() { 1 } else { 100 };
 
 /// `push_send_system` and `push_unsend_system` only check the `debug_name` field,
 /// so other fields can be left empty.
@@ -44,6 +44,12 @@ impl system::Unsendable for UnsendSystem {
 }
 
 struct Global1;
+struct Global2;
+
+#[comp(dynec_as(crate), of = TestArch)]
+struct Comp1;
+#[comp(dynec_as(crate), of = TestArch)]
+struct Comp2;
 
 /// Counts the number of times some node is unmarked as runnable.
 #[derive(Default)]
@@ -118,6 +124,36 @@ fn test_global_exclusion() {
     );
 }
 
+fn test_different_global_exclusion() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2], []| {
+            builder.use_resource(
+                Node::SendSystem(sys1),
+                ResourceType::Global(DbgTypeId::of::<Global1>()),
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys2),
+                ResourceType::Global(DbgTypeId::of::<Global2>()),
+                ResourceAccess { mutable: true, discrim: None },
+            );
+        },
+        || {
+            let asem = AntiSemaphore::new(2);
+            move |_| asem.wait()
+        },
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(unmark_count, 0, "Expected no resource exclusion on different globals");
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 2, "Expected 2 systems to run concurrently");
+        },
+    );
+}
+
 #[test]
 fn test_global_share() {
     test_bootstrap(
@@ -128,6 +164,287 @@ fn test_global_share() {
                 builder.use_resource(
                     Node::SendSystem(sys),
                     ResourceType::Global(DbgTypeId::of::<Global1>()),
+                    ResourceAccess { mutable: false, discrim: None },
+                );
+            }
+        },
+        || {
+            let asem = AntiSemaphore::new(2);
+            move |_| asem.wait()
+        },
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(unmark_count, 0, "Expected no exclusion");
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 2, "Expected 2 systems to run concurrently");
+        },
+    );
+}
+
+#[test]
+fn test_simple_exclusion() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2, sys3], []| {
+            builder.use_resource(
+                Node::SendSystem(sys1),
+                ResourceType::Simple {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys2),
+                ResourceType::Simple {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys3),
+                ResourceType::Simple {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: false, discrim: None }, // mutable: false here
+            );
+        },
+        || |_| (),
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(
+                unmark_count, 3,
+                "Expected resource exclusion to unmark all other runnable nodes"
+            );
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 1, "Expected resource exclusion to deny concurrency");
+        },
+    );
+}
+
+#[test]
+fn test_different_simple_exclusion() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2], []| {
+            builder.use_resource(
+                Node::SendSystem(sys1),
+                ResourceType::Simple {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys2),
+                ResourceType::Simple {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp2>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+        },
+        || {
+            let asem = AntiSemaphore::new(2);
+            move |_| asem.wait()
+        },
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(unmark_count, 0, "Expected no resource exclusion on different components");
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 2, "Expected 2 systems to run concurrently");
+        },
+    );
+}
+
+#[test]
+fn test_simple_share() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2, sys3, sys4], []| {
+            for sys in [sys1, sys2, sys3, sys4] {
+                builder.use_resource(
+                    Node::SendSystem(sys),
+                    ResourceType::Simple {
+                        arch: DbgTypeId::of::<TestArch>(),
+                        comp: DbgTypeId::of::<Comp1>(),
+                    },
+                    ResourceAccess { mutable: false, discrim: None },
+                );
+            }
+        },
+        || {
+            let asem = AntiSemaphore::new(2);
+            move |_| asem.wait()
+        },
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(unmark_count, 0, "Expected no exclusion");
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 2, "Expected 2 systems to run concurrently");
+        },
+    );
+}
+
+#[test]
+fn test_isotope_exclusion() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2, sys3], []| {
+            builder.use_resource(
+                Node::SendSystem(sys1),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys2),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys3),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: false, discrim: None }, // mutable: false here
+            );
+        },
+        || |_| (),
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(
+                unmark_count, 3,
+                "Expected resource exclusion to unmark all other runnable nodes"
+            );
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 1, "Expected resource exclusion to deny concurrency");
+        },
+    );
+}
+
+#[test]
+fn test_intersecting_isotope_exclusion() {
+    test_bootstrap(
+        3,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2, sys3], []| {
+            builder.use_resource(
+                Node::SendSystem(sys1),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: Some(vec![1, 2]) },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys2),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: Some(vec![2, 3]) },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys3),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: false, discrim: Some(vec![3, 4]) }, // mutable: false here
+            );
+        },
+        || {
+            let asem = AntiSemaphore::new(2);
+            move |node| {
+                if matches!(node, Node::SendSystem(SendSystemIndex(0 | 2))) {
+                    asem.wait();
+                }
+            }
+        },
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(
+                unmark_count, 1,
+                "Expected resource exclusion to unmark only exclusive nodes"
+            );
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(
+                max_concurrency, 2,
+                "Expected [1, 2] and [3, 4] systems to run concurrently"
+            );
+        },
+    );
+}
+
+#[test]
+fn test_different_isotope_exclusion() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2], []| {
+            builder.use_resource(
+                Node::SendSystem(sys1),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp1>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+            builder.use_resource(
+                Node::SendSystem(sys2),
+                ResourceType::Isotope {
+                    arch: DbgTypeId::of::<TestArch>(),
+                    comp: DbgTypeId::of::<Comp2>(),
+                },
+                ResourceAccess { mutable: true, discrim: None },
+            );
+        },
+        || {
+            let asem = AntiSemaphore::new(2);
+            move |_| asem.wait()
+        },
+        |(uct, mct)| {
+            let unmark_count = uct.0.into_inner();
+            assert_eq!(unmark_count, 0, "Expected no resource exclusion on different components");
+
+            let max_concurrency = mct.max.into_inner();
+            assert_eq!(max_concurrency, 2, "Expected 2 systems to run concurrently");
+        },
+    );
+}
+
+#[test]
+fn test_isotope_share() {
+    test_bootstrap(
+        2,
+        || (UnmarkCounterTracer::default(), MaxConcurrencyTracer::default()),
+        |builder, [sys1, sys2, sys3, sys4], []| {
+            for sys in [sys1, sys2, sys3, sys4] {
+                builder.use_resource(
+                    Node::SendSystem(sys),
+                    ResourceType::Isotope {
+                        arch: DbgTypeId::of::<TestArch>(),
+                        comp: DbgTypeId::of::<Comp1>(),
+                    },
                     ResourceAccess { mutable: false, discrim: None },
                 );
             }

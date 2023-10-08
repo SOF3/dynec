0eca06f597f1d0eb3bf5a541fe39e4f4b08e703e
SOFe <sofe2038@gmail.com>
2023-10-08 20:42:47 +0800

    bench(iter_entity): add benches with holes

--- benches/iter_entity_add.rs
+++ benches/iter_entity_add.rs
@@ -1,6 +1,7 @@
 use std::iter;
 use std::time::Duration;
 
+use criterion::measurement::WallTime;
 use criterion::*;
 use dynec::test_util::TestArch;
 use dynec::{comp, system};
@@ -121,60 +122,97 @@ fn system_array_add_system_chunked(
     }
 }
 
-fn bench_iter_entity_add<S: system::Sendable>(
+fn bench_iter_entity_add<SystemT, DeleteEntityIter>(
     group: &mut BenchmarkGroup<'_, measurement::WallTime>,
     function_name: &str,
-    build_system: impl Fn() -> S,
+    build_system: impl Fn() -> SystemT,
     make_comps: impl Fn(&mut ThreadRng) -> comp::Map<TestArch>,
-) {
+    entities_to_delete: impl Fn(u64) -> DeleteEntityIter,
+) where
+    SystemT: system::Sendable,
+    DeleteEntityIter: Iterator<Item = u64>,
+{
     group.measurement_time(Duration::from_secs(10));
 
     for log_entities in [12, 16] {
         let num_entities = 1 << log_entities;
         group.throughput(Throughput::Elements(num_entities));
         group.bench_with_input(
-            BenchmarkId::new(function_name, format!("{num_entities} entities")),
+            BenchmarkId::new(function_name, format!("{num_entities} ents")),
             &num_entities,
             |b, &num_entities| {
                 let mut world = dynec::system_test!(build_system(););
                 let mut rng = rand::thread_rng();
-                for _ in 0..num_entities {
-                    world.create(make_comps(&mut rng));
+                let mut entities: Vec<_> = (0..num_entities)
+                    .map(|_| world.create(make_comps(&mut rng)))
+                    .map(Some)
+                    .collect();
+                for pos in entities_to_delete(num_entities) {
+                    let entity = entities
+                        .get_mut(pos as usize)
+                        .expect("entities_to_delete yielded overflowing values");
+                    let entity = entity.take().expect("entities_to_delete yielded repeated values");
+                    world.delete(entity);
                 }
-                b.iter(|| {
-                    world.execute(&dynec::tracer::Noop);
-                })
+                b.iter(|| world.execute(&dynec::tracer::Noop))
             },
         );
     }
 }
 
-fn iter_entity_add(c: &mut Criterion) {
-    let mut group = c.benchmark_group("iter entity (p += v)");
+fn iter_entity_add_with_deletion<DeleteEntityIter: Iterator<Item = u64>>(
+    group: &mut BenchmarkGroup<WallTime>,
+    name: &str,
+    deletion: impl Fn(u64) -> DeleteEntityIter + Copy,
+) {
     bench_iter_entity_add(
-        &mut group,
-        "non-chunked (x,y,z)",
+        group,
+        &format!("{name} ent idv"),
         || system_individual_add_system_non_chunked.build(),
         individual_comps,
+        deletion,
     );
     bench_iter_entity_add(
-        &mut group,
-        "chunked (x,y,z)",
+        group,
+        &format!("{name} chunk idv"),
         || system_individual_add_system_chunked.build(),
         individual_comps,
+        deletion,
     );
     bench_iter_entity_add(
-        &mut group,
-        "non-chunked [f64; 3]",
+        group,
+        &format!("{name} ent arr"),
         || system_array_add_system_non_chunked.build(),
         array_comps,
+        deletion,
     );
     bench_iter_entity_add(
-        &mut group,
-        "chunked [f64; 3]",
+        group,
+        &format!("{name} chunk arr"),
         || system_array_add_system_chunked.build(),
         array_comps,
+        deletion,
     );
+}
+
+fn iter_entity_add(c: &mut Criterion) {
+    let mut group = c.benchmark_group("iter entity (p += v)");
+
+    iter_entity_add_with_deletion(&mut group, "full", |_| iter::empty());
+
+    iter_entity_add_with_deletion(&mut group, "holes", |total| {
+        iter::repeat([(1_u64, 2), (4, 8), (16, 12)].into_iter())
+            .flatten()
+            .scan(0, |state, (keep, delete)| {
+                *state += keep;
+                let start = *state;
+                *state += delete;
+                let end = *state;
+                Some(start..end)
+            })
+            .flatten()
+            .take_while(move |&index| index < total)
+    });
 }
 
 criterion_group!(benches, iter_entity_add);
